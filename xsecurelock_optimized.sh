cfe22f5fa308f460482965cd1d53600aba8bbda7
Scott R. Santos <santos@auburn.edu>
2023-02-02 00:00:52 -0500

    More tweaks

--- xsecurelock_optimized.sh
+++ xsecurelock_optimized.sh
@@ -3,16 +3,16 @@
 export XSECURELOCK_BACKGROUND_COLOR='#222222'
 export XSECURELOCK_AUTH_BACKGROUND_COLOR='#2e3436'
 export XSECURELOCK_AUTH_FOREGROUND_COLOR='#babdb6'
-export XSECURELOCK_AUTH_WARNING_COLOR='#700000'
+export XSECURELOCK_AUTH_WARNING_COLOR='#E5E500'
 ############################################################
 export XSECURELOCK_AUTH_CURSOR_BLINK=1
 export XSECURELOCK_AUTH_SOUNDS=0
-export XSECURELOCK_AUTH_TIMEOUT=10
+export XSECURELOCK_AUTH_TIMEOUT=7
 ############################################################
-export XSECURELOCK_BLANK_TIMEOUT=15
+export XSECURELOCK_BLANK_TIMEOUT=10
 export XSECURELOCK_BLANK_DPMS_STATE='off'
 ############################################################
-export XSECURELOCK_FONT='Roboto Mono for Powerline Regular'
+export XSECURELOCK_FONT='Roboto Mono for Powerline'
 export XSECURELOCK_PASSWORD_PROMPT=time_hex
 export XSECURELOCK_SHOW_USERNAME=1
 export XSECURELOCK_SHOW_HOSTNAME=2
@@ -25,7 +25,8 @@ export XSECURELOCK_VIDEOS_FLAGS='--osd-level=0 --load-scripts=no'
 export XSECURELOCK_IMAGE_DURATION_SECONDS=60
 ############################################################
 export XSECURELOCK_DIM_FPS=60
-export XSECURELOCK_DIM_TIME_MS=7000
+export XSECURELOCK_DIM_TIME_MS=5000
 export XSECURELOCK_WAIT_TIME_MS=5000
+export XSECURELOCK_SAVER_DELAY_MS=500
 
 /usr/bin/xsecurelock
