142e7583f6585a09999e0c57877374cd4ac2cebc
Scott R. Santos <santos@auburn.edu>
2023-02-01 23:24:56 -0500

    More tweaks

--- lock_w_xidlehook.sh
+++ lock_w_xidlehook.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 
-# Only exported variables can be used within the timer's command.
-export PRIMARY_DISPLAY="$(/usr/bin/xrandr | /usr/bin/awk '/ primary/{print $1}')"
-
 # Run xidlehook
 /usr/bin/xidlehook \
+  `# Set up communication socket` \
+  --socket /tmp/xidlehook.socket \
   `# Reset the idle timer after woken from suspend` \
   --detect-sleep \
   `# Don't lock when there's a fullscreen application` \
   --not-when-fullscreen \
   `# Don't lock when there's audio playing` \
   --not-when-audio \
-  `# Dim the screen after 3 mins, undim if user becomes active` \
-  --timer 180 \
-    '/usr/bin/xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
-    '/usr/bin/xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-  `# Undim & lock after 2 more mins` \
-  --timer 120 \
-    '/usr/bin/xrandr --output "$PRIMARY_DISPLAY" --brightness 1; /usr/bin/i3lock -f -b -i /home/srsantos/.i3/wallpapers/dark-metal-grids/dark-metal-grid-8_1600x900.png' \
+  `# Undim & lock at 10 min` \
+  --timer 600 \
+    '/usr/local/bin/dimmer && /usr/local/bin/xsecurelock_optimized.sh' \
     '' \
   `# Finally, suspend an hour after screen lock` \
   --timer 3600 \
