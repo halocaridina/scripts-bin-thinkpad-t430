4511e172ee340b0bf87d05d9da0b6aaf1cf84042
Scott R. Santos <santos@auburn.edu>
2023-02-05 09:49:50 -0500

    More tweaks

---
+++ lock_w_i3lock-color.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+TEXT='#D3D7CF'
+TEXT2='#000000'
+FONT='Iosevka Term SS03,Iosevka Term SS03 Regular'
+
+####################################################################
+# Prelock.....
+
+/usr/bin/systemd-cat -t "i3lock-color-pre" /usr/bin/echo "***** SCREEN LOCK READY TO ACTIVATE - $(date) *****"
+
+/usr/bin/killall -SIGUSR1 dunst
+
+####################################################################
+# Lock.....
+
+/usr/bin/i3lock \
+-n -f -b -i /home/srsantos/.i3/wallpapers/dark-metal-grids/dark-metal-grid-8_1600x900.png \
+--time-color=$TEXT --date-color=$TEXT --verif-color=$TEXT2 --wrong-color=$TEXT --modif-color=$TEXT \
+--layout-color=$TEXT --clock --indicator --time-str="%H:%M:%S" --date-str="%A, %Y-%m-%d" \
+--verif-text="Verifying..." --wrong-text="Wrong!" --noinput-text="No Input" --lock-text="Locking..." --lockfailed-text="Lock Failed!" \
+--greeter-color=$TEXT --greeter-pos="800:640" --greeter-text="" --time-font=$FONT --date-font=$FONT \
+--layout-font=$FONT --verif-font=$FONT --verif-size=26 --wrong-font=$FONT --greeter-font=$FONT \
+--pass-media-keys --pass-screen-keys --pass-power-keys --pass-volume-keys
+
+####################################################################
+#Postlock.....
+
+/usr/bin/killall -SIGUSR2 dunst
+
+/usr/bin/systemd-cat -t "i3lock-color-post" /usr/bin/echo "***** SCREEN LOCK DEACTIVATED - $(date) *****"
--- yubikey_removal_triggers_lock.sh
+++ yubikey_removal_triggers_lock.sh
@@ -4,9 +4,17 @@ export DISPLAY=:0
 export XAUTHORITY=/home/srsantos/.Xauthority
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
-if [ -z "$(lsusb | grep Yubikey)" ] ; then
+if [ -z "$(pidof Xorg)" ] ; then
+        /usr/bin/systemd-cat -t "yubikey-screen_lock-trigger" /usr/bin/echo "***** XORG NOT STARTED - YUBIKEY REMOVAL SCREEN LOCK NOT ACTIVATED - $(date) *****"
+        /usr/bin/pkill gpg-agent &>/dev/null
+        exit 0
+ elif [ -z "$(lsusb | grep Yubikey)" ] ; then
+        /usr/bin/systemd-cat -t "yubikey-screen_lock-trigger" /usr/bin/echo "***** YUBIKEY REMOVED - SCREEN LOCK ACTIVATED - $(date) *****"
         /usr/bin/xset dpms force off
+        /usr/bin/pkill gpg-agent &>/dev/null
+        exit 0
+ else
+        /usr/bin/pkill gpg-agent &>/dev/null
+        exit 0
 fi
-
-exit
 
